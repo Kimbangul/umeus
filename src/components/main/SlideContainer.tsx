01bab843d78d6d7191432e165ae9bcd8f7b796da
Kimbangul <highcolor_12@naver.com>
2023-12-07 14:55:32 +0900

    chore: install swiper

--- src/components/main/SlideContainer.tsx
+++ src/components/main/SlideContainer.tsx
@@ -1,7 +1,34 @@
+'use client'
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
+import { SwiperProps } from "swiper/react";
 import SlideView from "@/components/main/SlideView";
 
+
 const SlideContainer = () => {
-  return <SlideView />
+
+  const option :  SwiperProps  = {
+    className: "Slide__container",
+    modules:[Autoplay, Pagination, Navigation, EffectFade],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: false,
+      disableOnInteraction: true,
+    },
+    effect: 'fade',
+      fadeEffect: {
+      crossFade: true,
+
+    },
+    speed: 1200,
+    pagination: {
+      clickable: true
+    }
+  }
+  
+  return <SlideView option={option}/>
 }
 
 export default SlideContainer;
--- src/components/main/SlideView.tsx
+++ src/components/main/SlideView.tsx
@@ -1,7 +1,21 @@
-const SlideView = () => {
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
+import { SlideType } from "@/components/main/type";
+
+const SlideView :React.FC<SlideType> = ({option}) => {
   return(
     <section className="Slide">
-        
+        <Swiper {...option}>
+        <SwiperSlide className="Slide__slide">    
+          Slide1
+        </SwiperSlide>
+        <SwiperSlide className="Slide__slide">    
+          Slide2
+        </SwiperSlide>
+        </Swiper>
     </section>
   )
 }
