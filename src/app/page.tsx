02cae71815aa85110614f51ca18be172e4a723dc
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-12-08 16:12:25 +0900

    style: add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,19 @@ export default function Home() {
   return (
     <>
     <Slide/>
-    <section>
-      <h1>hello world</h1>
+    <section className="Main__section">
+      <div className="Main__inner">
+        <h2 className="Main__title">BEST ITEM</h2>
+        <ul className="Main__best-list">
+          <li className="Main__best-item">
+            <a href="#">
+              <div className="Main__best-img"></div>
+              <div className="Main__best-title">곰돌찡 토끼찡 얼굴 쿠션</div>
+              <div className="Main__best-price">24,000원</div>
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
     </>
   )
