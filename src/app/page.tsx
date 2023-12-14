200f9115b970822dd265e2b7226c2967bb723173
Kimbangul <highcolor_12@naver.com>
2023-12-14 11:13:19 +0900

    style: add category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Slide from "@/components/main/SlideContainer";
 
 export default function Home() {
+  const categoryData = [
+    {title: '신상품', icon:'', href:'#'},
+    {title: '인형/완구', icon:'', href:'#'},
+    {title: '핸드폰 악세사리', icon:'', href:'#'},
+    {title: '패션', icon:'', href:'#'},
+    {title: '키링', icon:'', href:'#'},
+    {title: '기타', icon:'', href:'#'},
+  ];
   const newItemData = [
     {img: '', title: '곰돌찡 토끼찡 얼굴 쿠션', price: 24000, href:''},
     {img: '', title: '곰돌찡 토끼찡 얼굴 쿠션', price: 24000, href:''},
@@ -13,6 +21,20 @@ export default function Home() {
   return (
     <>
     <Slide/>
+    <nav className="Main__cate">
+      <ul className="Main__cate-list">
+        {
+          categoryData.map((el,idx)=>
+            <li className="Main__cate-item" key={`cate-${idx}`}>
+              <a href={el.href}>
+                <div className="Main__cate-icon"></div>
+                <h2>{el.title}</h2>
+              </a>
+            </li>
+          )
+        }
+      </ul>
+    </nav>
     <section className="Main__section">
       <div className="Main__inner">
         <h2 className="Main__title">NEW ITEM</h2>
