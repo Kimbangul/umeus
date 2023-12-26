d5a96f4a6d4ef5b829bcc242158df7cfc438bff5
Kimbangul <highcolor_12@naver.com>
2023-12-26 09:08:51 +0900

    style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,6 +5,7 @@ export default function Custom404() {
   return (
   <Section>
     <div className="NotFound__img"></div>
+    <h1 className="NotFound__title">404 Not Found</h1>
     <p className="NotFound__desc">
       요청하신 페이지를 찾을 수 없습니다.<br />
       존재하지 않는 주소이거나 페이지가 삭제되었을 수 있습니다.
